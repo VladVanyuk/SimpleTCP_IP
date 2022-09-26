0d716a437a05ccd1c8c80953f534608e77c17f4c
Vladislav Vanyuk <43759007+VladVanyuk@users.noreply.github.com>
2022-09-26 17:50:35 +0300

    Added config file

---
+++ config.h
@@ -0,0 +1,7 @@
+#ifndef config_h
+#define config_h
+
+#define IP 0x7f000001
+#define PORT 8080
+
+#endif
--- server.c
+++ server.c
@@ -8,18 +8,19 @@
 #include <unistd.h>
 
 int main(int argc, char **argv)
-{    
+{   
     // create socket info struictures
     struct sockaddr_in server_info = {0};
-    
+    bzero((char *) &server_info,sizeof(server_info)); // it clears all the data to what it it reference to 
     server_info.sin_family = AF_INET;
-    server_info.sin_port = htons(1337);
+    server_info.sin_port = htons(8080);
 
     socklen_t server_info_len = sizeof(server_info);
 
     struct sockaddr client_info = {0};
     socklen_t client_info_len = sizeof(client_info);
-  
+    
+    char buffer[255]; 
 
     // create our socket
     int sfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -37,7 +38,7 @@ int main(int argc, char **argv)
     }
     
     // listen 
-    if (0 > listen(sfd, 0))
+S:    if (0 > listen(sfd, 0))
     {
         perror("listen");
         return -1;
@@ -52,9 +53,66 @@ int main(int argc, char **argv)
     }
 
     // do client stuff (send/recv)
-    char *hello = "Hello from Vlad\n";
-    ssize_t sent = send(cfd, hello, strlen(hello), 0);
+    double num1,num2, ans;
+    int choice;
+
+	num1 = write(cfd,"Enter Number 1 : ",strlen("Enter Number 1"));
+	if(num1 < 0)
+    {
+		perror("Error Writing to socket");
+        return -1;
+	}
+	read(cfd,&num1,sizeof(double));
+	printf("Client Number 1 is : %lf\n",num1);
+
+	num2 = write(cfd,"Enter Number 2 : ",strlen("Enter Number 2"));
+    if(num2 < 0)
+    {
+        perror("Error Writing to socket");
+        return -1;
+    }
+    read(cfd,&num2,sizeof(double));
+    printf("Client Number 2 is : %lf\n",num2);
+
+	choice = write(cfd,"Enter Your Choice\n1.Add\n2.Subtract\n3.Division\n4.Multiplication\n5.Turn off server\nelse:Exit",strlen("Enter Your Choice\n1.Add\n2.Subtract\n3.Division\n4.Multiplication\n5.Turn off server\nelse:Exit"));
+	if(choice < 0)
+    {
+		perror("Error on Writing");
+        return -1;
+	}
+	read(cfd,&choice,sizeof(int));
+    printf("Client - Choice is : %d\n",choice);
+	switch(choice)
+    {
+		case 1:
+			ans = num1 + num2;
+			break;
+		case 2:
+		    ans = num1-num2;
+			break;
+		case 3:
+		    ans = num1/num2;	
+			break;
+		case 4:
+			ans = num1*num2;
+			break;
+		case 5:
+            close(cfd);
+		    goto Q;
+	        break;
+        default:
+            printf("Choose one of whe choices!");
+            close(cfd);	       
+	}
+
+	write(cfd,&ans,sizeof(double)); 
+	if(choice != 5)
+    {
+		close(cfd);
+        goto S;
+	}
 
     // clean up
-    close(cfd);
+Q:  close(sfd);
+    return 0;
 }
